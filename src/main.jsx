d3d8c1e11aaa707eb852f4c467530294dd79489e
Sergei Iu <serg_omen@mail.ru>
2024-11-10 16:09:36 +0300

    (fix): analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-N7D1EMG1EJ");
+import GA4React from "ga-4-react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,7 +11,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
+
+// ReactGA.initialize("G-N7D1EMG1EJ");
+
+// ReactGA.send({
+//   hitType: "pageview",
+//   page: window.location.pathname,
+// });
+
+try {
+  setTimeout(_ => {
+    const ga4react = new GA4React("G-N7D1EMG1EJ");
+    ga4react.initialize().catch(err => console.error(err));
+  }, 4000);
+} catch (err) {
+      console.error(err);
+}
