dc916cdd9b5ee78db41f5fc3ad24a8cdd671cad0
Sergei Iu <serg_omen@mail.ru>
2024-11-10 12:04:36 +0300

    (fix): analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,15 @@ import App from './App';
 import './index.css';
 import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-N7D1EMG1EJ");
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-try {
-  setTimeout(_ => {
-    // const ga4react = new ReactGA("G-N7D1EMG1EJ");
-    ReactGA.initialize("G-N7D1EMG1EJ");
-  }, 4000);
-} catch (err) {
-      console.error(err);
-}
-
-// ReactGA.initialize("G-N7D1EMG1EJ");
-
-// ReactGA.send({
-//   hitType: "pageview",
-//   page: window.location.pathname,
-// });
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
